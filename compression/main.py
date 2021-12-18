6a9784b7f687c28fd1718cff09613585a49b3543
GenichiMaruo <gen1010f8e7@gmail.com>
2021-12-19 00:20:28 +0900

    ファイル入出力関連を追加

--- compression/main.py
+++ compression/main.py
@@ -8,8 +8,10 @@ files = sys.argv
 file = files[1]
 fr = open(file,'rb')
 
+
+
 if os.path.exists(file+'.pres'):
 	os.remove(file+'.pres')
-out_file = open(file+'.pres','w')
-out_file.write('abc')
+out_file = open(file+'.pres','wb')
+out_file.write(b'abc')
 out_file.close()
