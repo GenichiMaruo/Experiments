eeb10c9a505a093be36f950f3d47c4c0dba00292
GenichiMaruo <gen1010f8e7@gmail.com>
2021-12-19 22:52:30 +0900

    Dictionaryクラスにソート機能を実装

--- compression/encode.py
+++ compression/encode.py
@@ -1,9 +1,14 @@
 class Word:
     def __init__(self,word):
         self.word = word
-        self.count = 1    
+        self.count = 1
+
+    def set_word(self,word,count):
+        self.word = word
+        self.count = count 
 class Dictionary:
     words = []
+    sorted_words = []
 
     def __add_word(self, word):
         word_obj = Word(word)
@@ -16,10 +21,13 @@ class Dictionary:
         else:
             self.words[pos].count+=1
 
+    def sort_words(self):
+        self.sorted_words = sorted(self.words,key=lambda w: w.count)
+
 def encode(array):
     size = len(array)
     dictionary = Dictionary()
     for i in range(size-1):
         word = [array[i],array[i+1]]
         dictionary.check_and_add_word(word)
-
+    dictionary.sort_words()
