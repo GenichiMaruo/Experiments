1beb679c1f051d8f519210c3422466a0412ed831
GenichiMaruo <85728541+GenichiMaruo@users.noreply.github.com>
2021-12-21 15:03:52 +0900

    LZ78の実装完了

--- compression/encode.py
+++ compression/encode.py
@@ -1,37 +1,48 @@
 class Word:
-    def __init__(self, word, last_word_id, newnum, create_id):
+    def __init__(self, word, last_word_relative_pos, create_id):
         self.word = word
-        self.newnum = newnum
-        self.last_word_id = last_word_id
+        self.newnum = word[len(word)-1]
+        self.last_word_relative_pos = last_word_relative_pos
         self.create_id = create_id
 
 class Dictionary:
+    size = 255
+
     def __init__(self):
         self.words = []
-        self.word_count = 0
+        self.search_words = []
+        self.words_count = 0
 
-    def __add_word(self, word, last_word_id):
-        print(word, last_word_id, word[len(word)-1], self.word_count)
-        word_obj = Word(word, last_word_id, word[len(word)-1], self.word_count)
+    def __add_word(self, word, last_word_relative_pos):
+        word_obj = Word(word, last_word_relative_pos, self.words_count)
+        self.search_words.insert(0, word_obj)
+        if self.words_count >= self.size:
+            del self.search_words[self.size]
         self.words.append(word_obj)
-        self.word_count+=1
+        self.words_count += 1
 
     def check_and_add_word(self, word, id):
-        pos = next((wd for wd in self.words if wd.word==word), None)
+        pos = next((wd for wd in self.search_words if wd.word==word), None)
         if pos == None:
             self.__add_word(word, id)
             return 0
         else:
-            return pos.create_id
+            return self.words_count - pos.create_id
 
     def create_dictionary(self, filearray):
         word = []
         created_id = 0
         for i in range(filearray.size):
             word.append(filearray.array[i])
-            created_id = self.check_and_add_word(word,created_id)
+            created_id = self.check_and_add_word(word, created_id)
             if created_id == 0:
                 word = []
+
+    def export_dictionary(self):
+        export_array = []
+        for i in range(self.words_count):
+            export_array.extend([self.words[i].last_word_relative_pos,self.words[i].newnum])
+        return export_array
 
 class FileArray:
     def __init__(self, array):
@@ -42,4 +53,7 @@ def encode(array):
     filearray = FileArray(array)
     dictionary = Dictionary()
     dictionary.create_dictionary(filearray)
+    presarray = dictionary.export_dictionary()
+    print(len(array),'->',len(presarray))
+    return presarray
     
